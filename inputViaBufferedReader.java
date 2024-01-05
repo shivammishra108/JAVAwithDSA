bb070777555734a1bf61218c96140f710cbbb7cb
Shivam mishra <coder.mishrashivam@gmail.com>
2024-01-05 16:10:19 +0530

    Program to take the multiword input from user by using BufferdReader class.I imported the io package to use this class and use readLine() method to read input. I converd input in interger.

---
+++ inputViaBufferedReader.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class inputViaBufferedReader{
+    public static void main(String []args)throws IOException{
+        int num1,num2,sum;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the first interger: ");
+        num1 = Integer.parseInt(br.readLine());
+        System.out.println("Enter the second interger: ");
+        num2 = Integer.parseInt(br.readLine());
+        sum = num1 + num2;
+        System.out.println("Sum = "+ sum);
+    }
+}
